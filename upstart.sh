95b165b3cb16a6222133376f8683f30a2c462497
EJ Bensing <ebensing@rm-dash-r.com>
2013-09-02 04:15:41 +0000

    fixed some stuff in the upstart script

--- upstart.sh
+++ upstart.sh
@@ -4,8 +4,14 @@ cur_dir=$(pwd)
 
 escaped=${cur_dir//\//\\/}
 
-echo $escaped
+sed -e "s/_app_dir_/$escaped/" pylon.conf.template > pylon.conf.1
 
-sed -e "s/_app_dir_/$escaped/" pylon.conf.template > pylon.conf
+echo "What user should pylon run as?:"
+
+read user
+
+sed -e "s/_user_/$user/" pylon.conf.1 > pylon.conf
+
+rm pylon.conf.1
 
 sudo cp pylon.conf /etc/init/pylon.conf
