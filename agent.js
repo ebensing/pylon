e339d5799e02feab6518ebceaa5c7b13fd94e092
EJ Bensing <ebensing@rm-dash-r.com>
2013-09-02 19:28:25 -0700

    add ownership commands

--- agent.js
+++ agent.js
@@ -34,7 +34,8 @@ sock.on('deploy', function (cfg, url) {
     }
 
     var commands = cfg.after.map(function (item) {
-      return utils.format("cd %s && ", loc) + item.replace("%loc%", loc);
+      return utils.format("cd %s && ", loc) +
+              item.replace(/%loc%/g, loc).replace(/%user%/g, cfg.user);
     });
 
     async.eachSeries(commands, exec, function (err) {
--- config.js
+++ config.js
@@ -4,16 +4,18 @@ var settings = {};
 
 settings.projects = [
   {
-    "name" : "dspr",
-    "repo" : "ebensing/Dspr",
-    "branch" : "master",
-    "after" : [
+    name : "dspr",
+    repo : "ebensing/Dspr",
+    branch : "master",
+    after : [
       // okay, so this ugly ass PoS is because things need to run as root when
       // using upstart, and some node modules have trouble doing that when they
       // are installing. Should I just make upstart run as a normal user? most
       // likely. I'm lazy
-      'sudo -u root -i sh -c "cd %loc%; npm install"'
-    ]
+      'sudo -u root -i sh -c "cd %loc%; npm install"',
+      'sudo chown %user%:%user% %loc% -R'
+    ],
+    user : "ebensing"
   }
 ];
 
