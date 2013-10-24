df477624ce2b20d11e9dba4d36bdfe792e5133a1
EJ Bensing <ebensing@rm-dash-r.com>
2013-10-24 14:36:48 -0400

    config update

--- config.js
+++ config.js
@@ -10,11 +10,12 @@ settings.projects = [
     after : [
       // okay, so this ugly ass PoS is because things need to run as root when
       // using upstart, and some node modules have trouble doing that when they
-      // are installing. Should I just make upstart run as a normal user? most
+      // are installing. Should I just make upstart run as a normal user? Most
       // likely. I'm lazy
       'sudo -u root -i sh -c "cd %loc%; npm install"',
       'sudo chown %user%:%user% %loc% -R',
-      'sudo -u %user% -i sh -c "cd %loc%; forever start preprocessing/SearchController.js"'
+      'sudo -u %user% -i sh -c "forever stop client.js"',
+      'sudo -u %user% -i sh -c "cd %loc%; forever start client.js"'
     ],
     user : "ebensing"
   }
